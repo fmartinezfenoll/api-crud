b02c326a3d2908e8747b20c746c5a1ff7785cf5c
fmartinezfenoll <fmf29@gmail.com>
2024-02-05 13:00:28 +0100

    Primer servidor

---
+++ index.js
@@ -0,0 +1,10 @@
+var http = require('http');
+var server = http.createServer();
+function HTTP_Response(request, response) {
+ response.writeHead(200, {'Content-Type': 'text/plain'});
+ response.write('Hola, saludos desde la UA\n');
+ response.end();
+}
+server.on('request', HTTP_Response);
+server.listen(8080);
+console.log('Servidor ejecutándose en puerto 8080...');
