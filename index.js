5a21f0799bb265e35b37fad929806165a71a18d2
fmartinezfenoll <fmf29@gmail.com>
2024-02-11 20:15:00 +0100

    API REST CRUD (sin base de datos)

--- index.js
+++ index.js
@@ -1,14 +1,48 @@
 'use strict'
-const port = process.env.PORT || 8888;
+const port = process.env.PORT || 3000;
 const express = require('express');
 const logger = require('morgan');
 const app = express();
 // Declaramos los middleware
 app.use(logger('dev')); // probar con: tiny, short, dev, common, combined
-// Declaramos el API
-app.get('/hola/:name', (req, res) => {
-res.status(200).send({mensaje: `¡Hola ${req.params.name} desde SD con JSON!`});
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+// Implementamos el API RESTFul a través de los métodos
+app.get('/api/product', (req, res) => {
+res.status(200);
+res.send( {productos: []} );
 });
+app.get('/api/product/:productID', (req, res) => {
+const productID = req.params.productID;
+res.status(200);
+res.send({product: productID });
+});
+app.post('/api/product', (req, res) => {
+const queProducto = req.body;
+ console.log(queProducto);
+
+res.status(200);
+res.send({
+ mensaje: 'Producto creado',
+ producto: queProducto
+});
+});
+app.put('/api/product/:productID', (req, res) => {
+const queProducto = req.body;
+const productID = req.params.productID;
+res.status(200);
+res.send({
+ mensaje: `Se ha modificado el producto ${productID}`,
+ producto: queProducto
+});
+});
+app.delete('/api/product/:productID', (req, res) => {
+const productID = req.params.productID;
+
+res.status(200);
+res.send( { mensaje: `Se ha eliminado el producto ${productID}` } );
+});
+// Lanzamos nuestro servicio API
 app.listen(port, () => {
-console.log(`API REST ejecutándose en http://localhost:${port}/hola/:nombre`);
+ console.log(`API REST ejecutándose en http://localhost:${port}/api/product`);
 }); 
