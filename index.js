697d638e0b00cb1c0b1e65d29062c4a1c5172615
fmartinezfenoll <fmf29@gmail.com>
2024-02-12 16:42:47 +0100

    API REST CRUD con MongoDB

--- index.js
+++ index.js
@@ -1,48 +1,78 @@
 'use strict'
-const port = process.env.PORT || 3000;
+// declaraciones
+const port = process.env.PORT || 3000
 const express = require('express');
 const logger = require('morgan');
+const mongojs = require('mongojs');
 const app = express();
-// Declaramos los middleware
+var db = mongojs("SD"); // Enlazamos con la DB "SD"
+var id = mongojs.ObjectID; // Función para convertir un id textual en un objectID
+// middlewares
 app.use(logger('dev')); // probar con: tiny, short, dev, common, combined
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-// Implementamos el API RESTFul a través de los métodos
-app.get('/api/product', (req, res) => {
-res.status(200);
-res.send( {productos: []} );
+app.use(express.urlencoded({ extended: false })) // parse application/x-www-form-urlencoded
+app.use(express.json()) // parse application/json
+// añadimos un trigger previo a las rutas para dar soporte a múltiples colecciones
+app.param("coleccion", (req, res, next, coleccion) => {
+ console.log('param /api/:coleccion');
+ console.log('colección: ', coleccion);
+ req.collection = db.collection(coleccion);
+ return next();
 });
-app.get('/api/product/:productID', (req, res) => {
-const productID = req.params.productID;
-res.status(200);
-res.send({product: productID });
+// routes
+app.get('/api', (req, res, next) => {
+ console.log('GET /api');
+ console.log(req.collection);
+ db.getCollectionNames((err, colecciones) => {
+ if (err) return next(err);
+ res.json(colecciones);
+ });
 });
-app.post('/api/product', (req, res) => {
-const queProducto = req.body;
- console.log(queProducto);
-
-res.status(200);
-res.send({
- mensaje: 'Producto creado',
- producto: queProducto
+app.get('/api/:coleccion', (req, res, next) => {
+ req.collection.find((err, coleccion) => {
+ if (err) return next(err);
+ res.json(coleccion);
+ });
 });
+app.get('/api/:coleccion/:id', (req, res, next) => {
+ req.collection.findOne({ _id: id(req.params.id) }, (err, elemento) => {
+ if (err) return next(err);
+ res.json(elemento);
+ });
 });
-app.put('/api/product/:productID', (req, res) => {
-const queProducto = req.body;
-const productID = req.params.productID;
-res.status(200);
-res.send({
- mensaje: `Se ha modificado el producto ${productID}`,
- producto: queProducto
+app.post('/api/:coleccion', (req, res, next) => {
+ const elemento = req.body;
+ if (!elemento.nombre) {
+ res.status(400).json({
+ error: 'Bad data',
+ description: 'Se precisa al menos un campo <nombre>'
+ });
+ } else {
+ req.collection.save(elemento, (err, coleccionGuardada) => {
+ if (err) return next(err);
+ res.json(coleccionGuardada);
+ });
+ }
 });
+app.put('/api/:coleccion/:id', (req, res, next) => {
+ const elementoId = req.params.id;
+ const elementoNuevo = req.body;
+ req.collection.update(
+ { _id: id(elementoId) },
+ { $set: elementoNuevo },
+ { safe: true, multi: false },
+ (err, elementoModif) => {
+ if (err) return next(err);
+ res.json(elementoModif);
+ });
 });
-app.delete('/api/product/:productID', (req, res) => {
-const productID = req.params.productID;
-
-res.status(200);
-res.send( { mensaje: `Se ha eliminado el producto ${productID}` } );
+app.delete('/api/:coleccion/:id', (req, res, next) => {
+ const elementoId = req.params.id;
+ req.collection.remove({ _id: id(elementoId) }, (err, resultado) => {
+ if (err) return next(err);
+ res.json(resultado);
+ });
 });
-// Lanzamos nuestro servicio API
+// Iniciamos la aplicación
 app.listen(port, () => {
- console.log(`API REST ejecutándose en http://localhost:${port}/api/product`);
-}); 
+ console.log(`API REST ejecutándose en http://localhost:${port}/api/:coleccion/:id`);
+});
